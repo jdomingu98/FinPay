775eb3937f5985aaedf9fbe31801b6783ff8acd8
jdomingu98 <jadominguezarcas@gmail.com>
2025-10-05 17:41:21 +0200

    feat(service): added account-service

---
+++ account-service/src/test/java/com/finpay/account/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finpay.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
